0c48f4517164defdda0f9c965580d03e73bd8de4
vishalgaddam873 <vishal18@navgurukul.org>
2019-07-02 15:21:36 +0530

    Obstacles are created

--- obstacles.js
+++ obstacles.js
@@ -1,9 +1,9 @@
 class Obstacle {
   constructor(x, y, width, height) {
     var options = {
-        'density' : 0.3,
+        'density' : 2.0,
         'friction': 0.3,
-        'restitution':0.8
+        'restitution':0.3
     }
     this.body = Bodies.rectangle(x, y, width, height, options);
     this.width = width;
--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const { Engine, World, Bodies, Body} = Matter;
-const CANVAS_WIDTH = 600,
-      CANVAS_HEIGHT = 400;
+const CANVAS_WIDTH = 1280,
+      CANVAS_HEIGHT = 720;
 
 var engine, world;
-var ground;
-var obstacle1, obstacle2;
+var ground, base; // All static bodies
+var square1, square2,square3,square4, square5; // Square obstacles
+var rect1, rect2, rect3, rect4; // Recatngle obstacles
+
 function setup(){
   const canvas =  createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
   engine = Engine.create();
@@ -13,7 +15,7 @@ function setup(){
   var ground_options = {
     isStatic:true
   }
-  ground = Bodies.rectangle(width/2, height, width, 100, ground_options)
+  ground = Bodies.rectangle(width/2, height, width, 50, ground_options)
   World.add(world, ground);
 
   var box_options = {
@@ -22,8 +24,21 @@ function setup(){
   box = Bodies.rectangle(150, 100, 50, 50, box_options)
   World.add(world, box)
 
-  obstacle1 = new Obstacle(200,10,40,80);
-  obstacle2 = new Obstacle(190,130,40,80);
+  base = new Base(width - 250,height-35, 350, 20)
+
+  square1 = new Obstacle(width-350,height-80,80,80);
+  square2 = new Obstacle(width-150,height-80,80,80);
+  rect1 = new Obstacle(width-250,height-120,280,10);
+  square3 = new Obstacle(width-350,height-123,80,80);
+  square4 = new Obstacle(width-150,height-123,80,80);
+  rect2 = new Obstacle(width-250,height-125,280,10);
+  square5 = new Obstacle(width-250,height-150,80,80);
+  // rect3 = new Obstacle(width-250,height-125,280,10);
+  //
+  // rect4 =
+
+
+
 
 }
 
@@ -33,9 +48,13 @@ function draw(){
   noStroke(255);
   fill(170);
   rectMode(CENTER);
-  rect(ground.position.x, ground.position.y, width, 100);
-
-  obstacle1.show();
-  obstacle2.show();
-
+  rect(ground.position.x, ground.position.y, width, 50);
+  base.show();
+  square1.show();
+  square2.show();
+  rect1.show();
+  square3.show();
+  square4.show();
+  rect2.show()
+  square5.show()
 }
