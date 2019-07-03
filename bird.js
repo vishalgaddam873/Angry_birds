40221b4295d9684f1d26b34a78eba59731a1e962
vishalgaddam873 <vishal18@navgurukul.org>
2019-07-03 09:59:23 +0530

    Bird object is created

--- bird.js
+++ bird.js
@@ -1,26 +1,27 @@
-class Obstacle {
+class Bird {
   constructor(x, y, width, height) {
     var options = {
-        'density' : 1.3,
-        'friction': 1.0,
-        'restitution':0.5
-    }
+      'density':1.5,
+      'friction': 1.0,
+      'restitution':0.5
+    };
     this.body = Bodies.rectangle(x, y, width, height, options);
     this.width = width;
     this.height = height;
     World.add(world, this.body);
-  }
+  };
   show(){
-    var pos =this.body.position;
+    var pos = this.body.position;
     var angle = this.body.angle;
+
     push();
     translate(pos.x, pos.y);
     rotate(angle);
-    strokeWeight(4);
-    stroke("green");
-    fill(255);
-    rectMode(CENTER);
+    strokeWeight(3);
+    stroke('blue')
+    fill('red')
+    rectMode(CENTER)
     rect(0, 0, this.width, this.height);
     pop();
-  }
+  };
 };
--- sketch.js
+++ sketch.js
@@ -1,44 +1,67 @@
-const { Engine, World, Bodies, Body} = Matter;
+const { Engine, World, Bodies, Body, Mouse, MouseConstraint, Constraint} = Matter;
 const CANVAS_WIDTH = 720,
       CANVAS_HEIGHT = 520;
 
 var engine, world;
 var ground, base; // All static bodies
-var square1, square2,square3,square4, square5; // Square obstacles
-var rect1, rect2, rect3, rect4; // Recatngle obstacles
-var enemy1, enemy2; // Enemy's
-
+var square1, square2,square3,square4, square5; // Square obstacles objects
+var rect1, rect2, rect3, rect4; // Recatngle obstacles objects
+var enemy1, enemy2; // Enemy objects
+var bird; //Bird object
+var mConstraint
 function setup(){
   const canvas =  createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
   engine = Engine.create();
   world = engine.world;
 
+  // Createing ground
   ground = new Ground(width/2, height, width, 50);
 
+  // Base for obstacles in brown color
   base = new Base(width - 250,height-35, 350, 20)
 
+  // left Square obstacle
   square1 = new Obstacle(width-350,height-80,80,80);
 
+  // Lower Middle Enemy
   enemy1 = new Enemy(width-250, height-90,50,50);
 
+  // Right Square Obstacle
   square2 = new Obstacle(width-150,height-80,80,80);
 
+  // Lower Rectangle obstacle
   rect1 = new Obstacle(width-250,height-120,280,15);
 
+  // Middle Square obstacle
   square3 = new Obstacle(width-350,height-125,80,80);
 
+  // Middle Enemy
   enemy2 = new Enemy(width-250,height-125,50,50);
 
+  // Right Square Obstacle
   square4 = new Obstacle(width-150,height-125,80,80);
 
+  // Upper Rectangle Obstacle
   rect2 = new Obstacle(width-250,height-225,280,15);
 
+  // Upper Middle Square
   square5 = new Obstacle(width-250,height-250,80,80);
 
+  // Left Rod
   rect3 = new Obstacle(width-320, height-250,15,150)
 
+  // Right Rod
   rect4= new Obstacle(width-180,height-250,15,150);
 
+  // Bird Object
+  bird = new Bird(50,height-400,50,50);
+  var canvasMouse = Mouse.create(canvas.elt)
+  var options = {
+    mouse : canvasMouse
+  }
+  canvasMouse.pixelRatio = 2;
+  mConstraint = MouseConstraint.create(engine, options)
+  World.add(world, mConstraint )
 }
 
 function draw(){
@@ -66,4 +89,6 @@ function draw(){
   rect3.show();
   rect4.body.angle = 2.61799
   rect4.show();
+
+  bird.show();
 }
